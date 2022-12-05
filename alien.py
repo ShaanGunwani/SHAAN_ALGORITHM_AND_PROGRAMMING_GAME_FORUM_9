82e3cbfe8e290b8e67908066a25053a5bc4bcb31
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2022-12-05 22:15:56 +0700

    Update alien.py

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.image = pygame.image.load('Images/Alien.png')
+        self.image = pygame.image.load('Alien.png')
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
